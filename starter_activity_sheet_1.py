62779e6c235e6c05895257f180309f4050518bc7
WillRNZ1 <wr23078@mbc.school.nz>
2025-05-14 10:59:13 +1200

    Lesson 1 Finish

--- starter_activity_sheet_1.py
+++ starter_activity_sheet_1.py
@@ -1,16 +1,17 @@
 # starter program lesson 1
 
 # change to your name.
-name = "Sum Gai"
+breakfast= input("What did you have for breakfast? ")
+print(f"{breakfast} sounds great!")
+colour = input("What is your favourite colour? ")
+print(f"{colour} is a nice colour!")
+name = input('What is your name? ')
 print("We want to know if you like progamming!")
 print()
 # combine the next 2 lines into one commeand
-print("Do you like programming " + name + "?")
-answer = input()
+answer = input("Do you like programming " + name + "? ")
 
 # change to an f string
-print("Great! You said " + answer + "!")
+print(f"Great! You said {answer}!")
 
 print("Let's learn some Python today.")
-
-
