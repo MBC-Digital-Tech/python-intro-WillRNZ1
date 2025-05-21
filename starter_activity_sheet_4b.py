dae2e093fb05604adc079ae16132e377e20c78df
WillRNZ1 <wr23078@mbc.school.nz>
2025-05-22 10:33:17 +1200

    envelop

--- starter_activity_sheet_4b.py
+++ starter_activity_sheet_4b.py
@@ -7,7 +7,40 @@ def square():
         forward(100)
         right(90)
 
-square()
-left(45)
-square()
+
+
+def triangle():
+    for counter in range(2):
+        forward(140)
+        left(90)
+
+
+
+def pentagon():
+    for counter in range(5):
+        forward(100)
+        right(72)
+
+
+
+def hexagon():
+    for counter in range(6):
+        forward(100)
+        right(60)
+
+def rectangle():
+    for counter in range(2):
+        forward(200)
+        right(90)
+        forward(100)
+        right(90)
+
+def envelop():
+    rectangle()
+    right(45)
+    triangle()
+
+envelop()
+
+
 done()
